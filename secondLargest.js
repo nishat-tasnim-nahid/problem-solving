06daba65b90dbae48099d474f4bba1e2e7b1325d
Tasnim Nahid <nnnahidt@gmail.com>
2022-02-08 22:58:42 +0600

    solving secondLargest number find a array

--- secondLargest.js
+++ secondLargest.js
@@ -1,4 +1,5 @@
 // find the second largest number from a array
+
 function nextBiggest(arr) {
     let max = -Infinity, result = -Infinity;
   
@@ -6,9 +7,9 @@ function nextBiggest(arr) {
       const nr = Number(value)
   
       if (nr > max) {
-        [result, max] = [max, nr] // save previous max
+        [result, max] = [max, nr] 
       } else if (nr < max && nr > result) {
-        result = nr; // new second biggest
+        result = nr; 
       }
     }
   
